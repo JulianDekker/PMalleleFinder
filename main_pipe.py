c50ac3eb3a402855e77fdd16a266cc1682222127
[julian] <[Julian.dekker@live.nl]>
2020-03-09 11:00:04 +0100

    removed print

--- main_pipe.py
+++ main_pipe.py
@@ -120,7 +120,6 @@ def hap_seq(genelist):
             h1 = []
             for y in range(1, len(h)):
                 l1new = values
-                print(indices, h[y])
                 for i, j in zip(indices, h[y]):
                     l1new[i] = j
                 h1.append(''.join(l1new))
